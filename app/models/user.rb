a994844d8083df80d44e84e806b90a357a3d23a6
chrisvsanchez <chrisvsanchez@gmail.com>
2021-03-01 11:58:09 -0500

    added dependent destroy to prescriptions in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     has_many :acc_patients, foreign_key: :partner_id, class_name: "AccountabilityPartner"
     has_many :patients, through: :acc_patients
     # AccountabilityPartner Model to User Associations
-    has_many :prescriptions
+    has_many :prescriptions, :dependent => :destroy
     has_many :medications, through: :prescriptions
     #Validations 
     validates :name,:username, :email, :phone, :password,presence: true
