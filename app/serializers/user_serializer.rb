9823d3c98306470bc1a217896160d34a16abfebf
chrisvsanchez <chrisvsanchez@gmail.com>
2021-03-02 16:51:43 -0500

    added prescriptions and medications to serializer for user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :name, :username, :email, :phone, :profile_pic, :partners, :patients
+  attributes :name, :username, :email, :phone, :profile_pic, :partners, :patients, :medications, :prescriptions
 end
